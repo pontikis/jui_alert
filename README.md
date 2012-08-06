ddb7611d948ec9b5fce31fc01bd3c69cc23e390b
Christos Pontikis <pontikis@gmail.com>
2012-08-06 19:24:43 +0300

    Advanced options

--- README.md
+++ README.md
@@ -2,21 +2,38 @@ jui_alert
 =========
 
 <p>jui_alert is a jquery plugin which displays a user message (with timeout and close button)</p>
+<p>Copyright Christos Pontikis http://www.pontikis.gr License GPLv3</p>
 
 Usage:
 
+<p>HTML</p>
+<pre>
+<head>
+    <script type="text/javascript" src="/path/to/jquery.js"></script>
+    <script type="text/javascript" src="/path/to/jquery.jui_alert.js"></script>
+</head>
+</pre>
+
+<p>JS code</p>
 <pre>
 $("#element_id").jui_alert({
     message: 'your message',
     timeout: 3000,  // 0 cancels timer
-    class_btn_close: 'ui-icon ui-icon-closethick',  // default
+    class_btn_close: 'ui-icon ui-icon-closethick', // default
     class_btn_cancel_timer: 'ui-icon ui-icon-pin-s', // default
-    class_btn_cancel_timer_remove: 'ui-icon ui-icon-pin-s', // default
-    class_btn_cancel_timer_add: 'ui-icon ui-icon-pin-w', // default
-    class_alert: 'ui-state-highlight ui-corner-all'  // default
+    class_btn_cancel_timer_remove: 'ui-icon-pin-s', // default
+    class_btn_cancel_timer_add: 'ui-icon-pin-w', // default
+    class_icon: 'ui-icon ui-icon-alert', // default
+    class_widget: 'ui-widget', // default
+    class_alert: 'ui-state-error ui-corner-all', // default
+    style_message: 'text-align: left; margin-top: 10px; margin-bottom: 10px;', // default
+    style_icon: 'float: left; margin-top: 3px; margin-right: 5px;', // default
+    style_alert: 'padding: 0 .7em;', // default
+    style_btn_close: 'float: right;', // default
+    style_btn_cancel_timer: 'float: right;' // default
 })
 </pre>
 
-Default css class need jquery-ui Themes CSS.
+Default styling needs jquery-ui Themes CSS: http://jqueryui.com/docs/Theming
 
-JSFIDDLE http://jsfiddle.net/pontikis/YXT3r/
+JSFIDDLE demo http://jsfiddle.net/pontikis/YXT3r/
--- jquery.jui_alert.js
+++ jquery.jui_alert.js
@@ -2,7 +2,7 @@
  * jquery plugin which displays a user message with timeout and close button
  * Requires jquery, jquery-ui Themes CSS
  * Copyright 2012 Christos Pontikis (http://www.pontikis.gr)
- * https://github.com/pontikis/jui_alert
+ * Project page https://github.com/pontikis/jui_alert
  * License GPLv3
  */
 (function ($) {
@@ -12,9 +12,16 @@
             timeout: 2500,
             class_btn_close: 'ui-icon ui-icon-closethick',
             class_btn_cancel_timer: 'ui-icon ui-icon-pin-s',
-            class_btn_cancel_timer_remove: 'ui-icon ui-icon-pin-s',
-            class_btn_cancel_timer_add: 'ui-icon ui-icon-pin-w',
-            class_alert: 'ui-state-highlight ui-corner-all'
+            class_btn_cancel_timer_remove: 'ui-icon-pin-s',
+            class_btn_cancel_timer_add: 'ui-icon-pin-w',
+            class_icon: 'ui-icon ui-icon-alert',
+            class_widget: 'ui-widget',
+            class_alert: 'ui-state-error ui-corner-all',
+            style_message: 'text-align: left; margin-top: 10px; margin-bottom: 10px;',
+            style_icon: 'float: left; margin-top: 3px; margin-right: 5px;',
+            style_alert: 'padding: 0 .7em;',
+            style_btn_close: 'float: right;',
+            style_btn_cancel_timer: 'float: right;'
         };
 
         var options = $.extend(defaults, options);
@@ -25,17 +32,24 @@
         var btn_close_id = 'jui_alert_btn_close_' + instance;
         var btn_cancel_timer_id = 'jui_alert_btn_cancel_timer_' + instance;
 
-        var html_btn_close = '<a id="' + btn_close_id + '" href="javascript:void(0);" class="' + options.class_btn_close + '" style="float: right;">close</a>';
-        var html_btn_clear_timer = '<a id="' + btn_cancel_timer_id + '" href="javascript:void(0);" class="' + options.class_btn_cancel_timer + '" style="float: right;">clear timer</a>';
+        var html1 = '<div class="' + options.class_alert + '" style="' + options.style_alert + '">';
+        var html2 = '<p style="' + options.style_message + '"><span class="' + options.class_icon + '" style="' + options.style_icon + '"></span>';
+        var html3 = '</p>';
+        var html4 = '</div>';
+
+        var html_btn_close = '<a id="' + btn_close_id + '" href="javascript:void(0);" class="' + options.class_btn_close + '" style="' + options.style_btn_close + '">close</a>';
+        var html_btn_clear_timer = '<a id="' + btn_cancel_timer_id + '" href="javascript:void(0);" class="' + options.class_btn_cancel_timer + '" style="' + options.style_btn_cancel_timer + '">clear timer</a>';
         if (options.timeout == 0) {
             html_btn_clear_timer = '';
         }
+
         return this.each(function () {
             obj = $(this);
             var current_obj_id = $(this).attr("id");
 
-            obj.html(html_btn_close + html_btn_clear_timer + options.message);
-            obj.addClass(options.class_alert);
+            obj.addClass(options.class_widget);
+            obj.html(html1 + html_btn_close + html_btn_clear_timer + html2 + options.message + html3 +html4);
+
             if (options.timeout > 0) {
                 message_tm = setTimeout(function () {
                     $("#" + btn_close_id).click();
